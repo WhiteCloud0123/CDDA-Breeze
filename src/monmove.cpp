69bdf8c79f3e5cf41e7616ac6306a9406c5cc305
WhiteCloud0123 <112397151+WhiteCloud0123@users.noreply.github.com>
2023-02-07 22:50:46 +0800

    Update monmove.cpp

--- src/monmove.cpp
+++ src/monmove.cpp
@@ -2245,7 +2245,7 @@ void monster::shove_vehicle( const tripoint &remote_destination,
                 // 标记 如果玩家没有PUSH_VEH_NO_DAMAGE 这个flag，那么载具不会因为怪物的推动行为本身受到伤害，照常接受其他伤害
                if( this->has_flag( MF_PUSH_VEH ) == false ) {
                     
-                    shove_damage_max=0.00f;                 
+                    shove_damage_max=0.00F;                 
 
                } 
                 veh.smash( here, shove_damage_min, shove_damage_max, 0.10F );
