2e37a8263f85b99fa9266b9ab73f12e1f8a3f1db
WhiteCloud0123 <112397151+WhiteCloud0123@users.noreply.github.com>
2023-03-26 17:53:02 +0800

    手动补全安卓资源文件

--- android/app/src/main/assets/lang/string_extractor/parsers/start_location.py
+++
@@ -1,6 +0,0 @@
-from ..write_text import write_text
-
-
-def parse_start_location(json, origin):
-    write_text(json["name"], origin,
-               comment="Name of starting location \"{}\"".format(json["id"]))
