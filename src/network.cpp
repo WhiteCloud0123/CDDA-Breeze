cdcac6ca08f35c88cac76f5efaeb3a863c8326d9
WhiteCloud0123 <112397151+WhiteCloud0123@users.noreply.github.com>
2023-07-17 23:19:40 +0800

    添加一个简单的网络测试

---
+++ src/network.cpp
@@ -0,0 +1,6 @@
+#include "network.h"
+
+ asio2::udp_server server_breeze;
+ asio2::udp_client client_breeze;
+ bool is_server = false;
+ bool is_client = false;
---
+++ src/network.h
@@ -0,0 +1,18 @@
+#pragma once
+#ifndef CATA_SRC_NETWORK_H
+#define CATA_SRC_NETWORK_H
+
+#include<asio2/asio2.hpp>
+
+
+
+    extern asio2::udp_server server_breeze;
+    extern asio2::udp_client client_breeze;
+    extern bool is_server;
+    extern bool is_client;
+
+
+
+
+
+#endif // CATA_SRC_NETWORK_H
