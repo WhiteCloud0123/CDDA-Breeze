d2e5bb3f0c18a74253806c297980ce7a4e86afbd
WhiteCloud0123 <112397151+WhiteCloud0123@users.noreply.github.com>
2024-02-18 22:35:17 +0800

    使用更高效的容器维护安卓快捷键映射

--- src/sdltiles.cpp
+++ src/sdltiles.cpp
@@ -2,6 +2,7 @@
 #include "sdltiles.h" // IWYU pragma: associated
 
 #include "cuboid_rectangle.h"
+#include "parallel_hashmap/phmap.h"
 #include "point.h"
 
 #if defined(TILES)
@@ -2038,7 +2039,7 @@ static bool needs_sdl_surface_visibility_refresh = true;
 
 // Quick shortcuts container: maps the touch input context category (std::string) to a std::list of input_events.
 using quick_shortcuts_t = std::list<input_event>;
-std::map<std::string, quick_shortcuts_t> quick_shortcuts_map;
+phmap::btree_map<std::string, quick_shortcuts_t> quick_shortcuts_map;
 
 // A copy of the last known input_context from the input manager. It's important this is a copy, as there are times
 // the input manager has an empty input_context (eg. when player is moving over slow objects) and we don't want our
