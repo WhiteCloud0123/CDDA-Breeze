51f29abd175849bfac8619587db43dd3f966fbda
WhiteCloud0123 <112397151+WhiteCloud0123@users.noreply.github.com>
2024-04-17 22:17:52 +0800

    ..

--- src/breeze.h
+++ src/breeze.h
@@ -1,24 +1,4 @@
 #pragma once
 
-#include "cxxpool/cxxpool.h"
-#include <filesystem>
 
-namespace breeze {
 
-    namespace thread_pool {
-
-        extern cxxpool::thread_pool pool;
-
-    }
-
-    namespace filesyetm {
-
-        int copy_(std::filesystem::path& path);
-
-        int delete_(std::filesystem::path& path);
-
-    }
-
-    
-
-}
