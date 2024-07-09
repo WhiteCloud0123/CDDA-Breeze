1f1e92f8b256c60b5947550a38e02b30d3d7e731
WhiteCloud0123 <112397151+WhiteCloud0123@users.noreply.github.com>
2024-07-09 16:59:11 +0800

    附魔 近战 酸液

--- src/item.cpp
+++ src/item.cpp
@@ -2326,6 +2326,7 @@ void item::enchantment_info(std::vector<iteminfo>& info, const iteminfo_query* p
         double per = 0.0;
         double item_damage_bash = 0.0;
         double item_damage_cut = 0.0;
+        double item_damage_acid = 0.0;
         double item_damage_heat = 0.0;
         double armor_bash = 0.0;
         double armor_cut = 0.0;
@@ -2347,6 +2348,7 @@ void item::enchantment_info(std::vector<iteminfo>& info, const iteminfo_query* p
         double per_mult = 1.0;
         double item_damage_bash_mult = 1.0;
         double item_damage_cut_mult = 1.0;
+        double item_damage_acid_mult = 1.0;
         double item_damage_heat_mult = 1.0;
         double armor_bash_mult = 1.0;
         double armor_cut_mult = 1.0;
@@ -2374,6 +2376,7 @@ void item::enchantment_info(std::vector<iteminfo>& info, const iteminfo_query* p
             per += e.get_value_add(enchant_vals::mod::PERCEPTION);
             item_damage_bash += e.get_value_add(enchant_vals::mod::ITEM_DAMAGE_BASH);
             item_damage_cut += e.get_value_add(enchant_vals::mod::ITEM_DAMAGE_CUT);
+            item_damage_acid += e.get_value_add(enchant_vals::mod::ITEM_DAMAGE_ACID);
             item_damage_heat += e.get_value_add(enchant_vals::mod::ITEM_DAMAGE_HEAT);
             armor_bash += e.get_value_add(enchant_vals::mod::ARMOR_BASH);
             armor_cut += e.get_value_add(enchant_vals::mod::ARMOR_CUT);
@@ -2395,6 +2398,7 @@ void item::enchantment_info(std::vector<iteminfo>& info, const iteminfo_query* p
             per_mult += e.get_value_multiply(enchant_vals::mod::PERCEPTION);
             item_damage_bash_mult += e.get_value_multiply(enchant_vals::mod::ITEM_DAMAGE_BASH);
             item_damage_cut_mult += e.get_value_multiply(enchant_vals::mod::ITEM_DAMAGE_CUT);
+            item_damage_acid_mult += e.get_value_multiply(enchant_vals::mod::ITEM_DAMAGE_ACID);
             item_damage_heat_mult += e.get_value_multiply(enchant_vals::mod::ITEM_DAMAGE_HEAT);
             armor_bash_mult += e.get_value_multiply(enchant_vals::mod::ARMOR_BASH);
             armor_cut_mult += e.get_value_multiply(enchant_vals::mod::ARMOR_CUT);
@@ -2421,14 +2425,14 @@ void item::enchantment_info(std::vector<iteminfo>& info, const iteminfo_query* p
 
         if (resonance != 0.0 || pain != 0.0 || speed != 0.0 || base_move_cost != 0.0 || attack_cost != 0.0 || regen_mana != 0.0
             || str != 0.0 || dex != 0.0 || inte != 0.0 || per != 0.0
-            || item_damage_heat !=0.0 || item_damage_bash != 0.0 || item_damage_cut != 0.0
+            || item_damage_heat !=0.0 || item_damage_bash != 0.0 || item_damage_cut != 0.0 || item_damage_acid !=0.0
             ||armor_bash != 0.0 || armor_cut != 0.0 || armor_stab != 0.0
             ||armor_bullet !=0.0|| armor_elec != 0.0 || armor_acid != 0.0 
             || armor_heat != 0.0 
             || resonance_mult != 1.0 ||pain_mult !=1.0 || speed_mult != 1.0 || base_move_cost_mult != 1.0 || attack_cost_mult != 1.0 
             || regen_mana_mult !=1.0
             || str_mult != 1.0 || dex_mult != 1.0 || inte_mult != 1.0 || per_mult != 1.0
-            || item_damage_heat_mult != 1.0 || item_damage_bash_mult !=1.0 || item_damage_cut_mult != 1.0
+            || item_damage_heat_mult != 1.0 || item_damage_bash_mult !=1.0 || item_damage_cut_mult != 1.0 || item_damage_acid_mult !=1.0
             || armor_bash_mult != 1.0 || armor_cut_mult != 1.0 || armor_stab_mult != 1.0
             || armor_bullet_mult != 1.0 || armor_elec_mult != 1.0 || armor_acid_mult != 1.0
             || armor_heat_mult != 1.0
@@ -2628,8 +2632,8 @@ void item::enchantment_info(std::vector<iteminfo>& info, const iteminfo_query* p
             
             
             if (
-                item_damage_heat != 0.0 || item_damage_bash != 0.0 || item_damage_cut != 0.0
-                || item_damage_heat_mult != 1.0 || item_damage_bash_mult !=1.0 || item_damage_cut_mult !=1.0
+                item_damage_heat != 0.0 || item_damage_bash != 0.0 || item_damage_cut != 0.0 || item_damage_acid !=0.0
+                || item_damage_heat_mult != 1.0 || item_damage_bash_mult !=1.0 || item_damage_cut_mult !=1.0 || item_damage_acid_mult !=1.0
                 ) {
 
                 info.emplace_back("DESCRIPTION", "* 近战伤害：");
@@ -2668,6 +2672,25 @@ void item::enchantment_info(std::vector<iteminfo>& info, const iteminfo_query* p
                     }
                 }
                 if (base_str != "    斩击：") {
+                    info.emplace_back("DESCRIPTION",
+                        string_format("%s", base_str));
+                }
+
+                base_str = "    酸液：";
+                need_space = false;
+                if (item_damage_acid != 0.0) {
+                    base_str += string_format("<color_c_yellow>%d</color>", static_cast<int>(item_damage_acid));
+                    need_space = true;
+                }
+                if (item_damage_acid_mult != 1.0) {
+                    if (need_space) {
+                        base_str += string_format("   <color_c_yellow>%.2f</color>", item_damage_acid_mult);
+                    }
+                    else {
+                        base_str += string_format("<color_c_yellow>%.2f</color>", item_damage_acid_mult);
+                    }
+                }
+                if (base_str != "    酸液：") {
                     info.emplace_back("DESCRIPTION",
                         string_format("%s", base_str));
                 }
