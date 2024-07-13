d71cc7981f1414e2c43cb56593a8d700f3a8418c
WhiteCloud0123 <112397151+WhiteCloud0123@users.noreply.github.com>
2024-07-13 19:09:23 +0800

    脚步声

--- src/item.cpp
+++ src/item.cpp
@@ -2326,6 +2326,7 @@ void item::enchantment_info(std::vector<iteminfo>& info, const iteminfo_query* p
         double carry_weight = 0.0;
         double climate_control_heat = 0.0;
         double climate_control_chill = 0.0;
+        double footstep_noise = 0.0;
         double str = 0.0;
         double dex = 0.0;
         double inte = 0.0;
@@ -2353,6 +2354,7 @@ void item::enchantment_info(std::vector<iteminfo>& info, const iteminfo_query* p
         double carry_weight_mult = 1.0;
         double climate_control_heat_mult = 1.0;
         double climate_control_chill_mult = 1.0;
+        double footstep_noise_mult = 1.0;
         double str_mult = 1.0;
         double dex_mult = 1.0;
         double inte_mult = 1.0;
@@ -2409,6 +2411,7 @@ void item::enchantment_info(std::vector<iteminfo>& info, const iteminfo_query* p
             carry_weight += e.get_value_add(enchant_vals::mod::CARRY_WEIGHT);
             climate_control_heat += e.get_value_add(enchant_vals::mod::CLIMATE_CONTROL_HEAT);
             climate_control_chill += e.get_value_add(enchant_vals::mod::CLIMATE_CONTROL_CHILL);
+            footstep_noise += e.get_value_add(enchant_vals::mod::FOOTSTEP_NOISE);
             str += e.get_value_add(enchant_vals::mod::STRENGTH);
             dex += e.get_value_add(enchant_vals::mod::DEXTERITY);
             inte += e.get_value_add(enchant_vals::mod::INTELLIGENCE);
@@ -2436,6 +2439,7 @@ void item::enchantment_info(std::vector<iteminfo>& info, const iteminfo_query* p
             carry_weight_mult += e.get_value_multiply(enchant_vals::mod::CARRY_WEIGHT);
             climate_control_heat_mult += e.get_value_multiply(enchant_vals::mod::CLIMATE_CONTROL_HEAT);
             climate_control_chill_mult += e.get_value_multiply(enchant_vals::mod::CLIMATE_CONTROL_CHILL);
+            footstep_noise_mult += e.get_value_multiply(enchant_vals::mod::FOOTSTEP_NOISE);
             str_mult += e.get_value_multiply(enchant_vals::mod::STRENGTH);
             dex_mult += e.get_value_multiply(enchant_vals::mod::DEXTERITY);
             inte_mult += e.get_value_multiply(enchant_vals::mod::INTELLIGENCE);
@@ -2477,6 +2481,7 @@ void item::enchantment_info(std::vector<iteminfo>& info, const iteminfo_query* p
                
         if (resonance != 0.0 || pain != 0.0 || speed != 0.0 || base_move_cost != 0.0 || attack_cost != 0.0 || max_mana !=0.0 
             || regen_mana != 0.0|| carry_weight !=0.0 || climate_control_heat != 0.0 || climate_control_chill != 0.0 
+            || footstep_noise !=0.0
             || str != 0.0 || dex != 0.0 || inte != 0.0 || per != 0.0
             || item_damage_heat !=0.0 || item_damage_bash != 0.0 || item_damage_cut != 0.0 || item_damage_acid !=0.0 
             || item_damage_cold != 0.0
@@ -2488,6 +2493,7 @@ void item::enchantment_info(std::vector<iteminfo>& info, const iteminfo_query* p
             || resonance_mult != 1.0 ||pain_mult !=1.0 || speed_mult != 1.0 || base_move_cost_mult != 1.0 || attack_cost_mult != 1.0 
             || max_mana_mult != 1.0  || regen_mana_mult != 1.0 || carry_weight_mult !=1.0
             || climate_control_heat_mult != 1.0 || climate_control_chill_mult != 1.0
+            || footstep_noise_mult !=1.0
             || str_mult != 1.0 || dex_mult != 1.0 || inte_mult != 1.0 || per_mult != 1.0            
             || item_damage_heat_mult != 1.0 || item_damage_bash_mult !=1.0 || item_damage_cut_mult != 1.0 || item_damage_acid_mult !=1.0 
             || item_damage_cold_mult != 1.0           
@@ -2700,6 +2706,25 @@ void item::enchantment_info(std::vector<iteminfo>& info, const iteminfo_query* p
                 }
             }
             if (base_str != "* 使体温向舒适的方向下降的能力：") {
+                info.emplace_back("DESCRIPTION",
+                    string_format("%s", base_str));
+            }
+
+            base_str = "* 脚步声：";
+            need_space = false;
+            if (footstep_noise != 0.0) {
+                base_str += string_format("<color_c_yellow>%d</color>", static_cast<int>(footstep_noise));
+                need_space = true;
+            }
+            if (footstep_noise_mult != 1.0) {
+                if (need_space) {
+                    base_str += string_format("   <color_c_yellow>x %.2f</color>", footstep_noise_mult);
+                }
+                else {
+                    base_str += string_format("<color_c_yellow>x %.2f</color>", footstep_noise_mult);
+                }
+            }
+            if (base_str != "* 脚步声：") {
                 info.emplace_back("DESCRIPTION",
                     string_format("%s", base_str));
             }
