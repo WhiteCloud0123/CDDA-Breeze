c76cce5c03d3a1958a2f0e4b61a6bcd0fead95fc
WhiteCloud0123 <112397151+WhiteCloud0123@users.noreply.github.com>
2024-10-12 22:56:12 +0800

    DDA端-来自adamaxis：修复npc制作过程可能导致游戏出现无响应的情况
    Co-Authored-By: adamaxis <44177929+adamaxis@users.noreply.github.com>

--- src/activity_actor.cpp
+++ src/activity_actor.cpp
@@ -3090,6 +3090,7 @@ void craft_activity_actor::do_turn( player_activity &act, Character &crafter )
     const int assistants = crafter.available_assistant_count( craft.get_making() );
 
     if( crafting_speed <= 0.0f ) {
+        craft.erase_var("crafter");
         crafter.cancel_activity();
         return;
     }
@@ -3164,6 +3165,7 @@ void craft_activity_actor::do_turn( player_activity &act, Character &crafter )
         if( !crafter.craft_consume_tools( craft, five_percent_steps, false ) ) {
             // So we don't skip over any tool comsuption
             craft.item_counter -= craft.item_counter % 500'000 + 1;
+            craft.erase_var("crafter");
             crafter.cancel_activity();
             return;
         }
